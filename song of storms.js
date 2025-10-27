237cca1e91c1851a78eec0fa0d0b8ab477214b2a
Dustin Dobransky <3844590+Inventor22@users.noreply.github.com>
2025-10-27 11:21:31 -0700

    Display additional cycles on the pianoroll

--- song of storms.js
+++ song of storms.js
@@ -4,20 +4,21 @@
 
 setcpm(140*2.5)
 
+const instruments = ["gm_accordion", "piano", "gm_harmonica", "gm_ocarina"]
+const melody_instrument = instruments[0]
+const base_instrument = instruments[1]
+
 stack(
   note(`<
     d4 a4 d5@4 d4 a4 d5@4 e5@3 f5 e5 f5 e5 c5 a4@4 a4@2 d4@2 f4 g4 a4@6 a4@2 d4@2 f4 g4 e4@6 
     d4 a4 d5@4 d4 a4 d5@4 e5@3 f5 e5 f5 e5 c5 a4@4 a4@2 d4@2 f4 g4 a4@4 a4@2 d4@12
   >`)
-  //.jux(press)
-  //._punchcard({labels:1})
+  .sound(melody_instrument)
+  ._pianoroll({labels:1, cycles:32})
   ,
   note(`<d3@2 [F3,A3]@2 [F3,A3]@2 d3@2 [G3,B3]@4 d3@2 [a3,c4]@2 [a3,c4]@2 d3@2 [g3,b3]@4>`)
-  .gain(.9)
-  //.jux(rev)
-  //._pianoroll()
+  .gain(.6)
+  .sound(base_instrument)
+  ._pianoroll({labels:1, cycles:32})
 )
-.sound("piano")
-.release(0.7)
-//.room(1).roomsize(2)
-//.jux(rev)
+.room(.5).roomsize(2)
